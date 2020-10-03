e8faa4fc68bd842eea2781cd891b466e5c21ef81
techClubMember <jesserussell411@gmail.com>
2020-10-02 22:18:06 -0600

    Improved documentation and renamed CustomFunction.Definition to CustomFunction.Behavior

--- ExoParseV2/functions/Function.cs
+++ ExoParseV2/functions/Function.cs
@@ -6,17 +6,35 @@ using ParsingTools;
 
 namespace ExoParseV2
 {
+    /// <summary>
+    /// Abstract class that represents a mathimematical function with an arbitrary number of inputs and one output.
+    /// </summary>
     public abstract class Function
     {
         public abstract string Name { get; }
+        /// <summary>
+        /// Inputs
+        /// </summary>
         public abstract string[] Parameters { get; }
+        /// <summary>
+        /// Number of Inputs
+        /// </summary>
         public virtual int ParamCount { get { return Parameters.Length; } }
+        /// <summary>
+        /// Unique identifier for this function using it's name and parameter count.
+        /// </summary>
         public (string name, int paramCount) Id { get { return (name: Name, paramCount: ParamCount); } }
 
         public IElement Calculate(params double?[] args)
         {
             return Calculate(args.Select(a => a.ToElement()).ToArray());
         }
+
+        /// <summary>
+        /// Executes the function with the given input arguments. IF the number of arguments given does not match the number of parametes, null will be returned.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public virtual IElement Calculate(params IElement[] args)
         {
             if (args.Length != ParamCount)
@@ -50,28 +68,47 @@ namespace ExoParseV2
         }
     }
 
+
+    /// <summary>
+    /// A function who's behavior is defined by a given IElement object. Links to the functions parameters are given as Variable objects that are assumed to exist in the given IElement behavior object.
+    /// </summary>
     public class CustomFunction : Function
     {
         public override string Name { get; }
-        public override String[] Parameters { get; }
-        public IElement Definition { get; }
+        /// <summary>
+        /// Inputs
+        /// </summary>
+        public override string[] Parameters { get; }
+        /// <summary>
+        /// Defines the function's behavior.
+        /// </summary>
+        public IElement Behavior { get; }
+
+        /// <summary>
+        /// Links to Variable objects assumed to exist in the IElement object defining the function's behavior.
+        /// </summary>
         public Variable[] ArgVars { get; }
         
         public CustomFunction(String name, IElement definition, Variable[] arguments)
         {
             Name = name;
-            Definition = definition;
+            Behavior = definition;
             ArgVars = arguments;
-            Parameters = ArgVars.Select(v => v.Name).ToArray();
+            Parameters = ArgVars.Select(v => v.Name).ToArray(); // Set Parameters acordingly.
         }
 
         protected override IElement calc(IElement[] args)
         {
-            for(int i = 0; i < ArgVars.Length; i++)
+            // Set the value for each parameter to the given value.
+            //Note that args is guaranteed to have the same length as ArgVars thanks to the actual interface method: Function.Calculate which calls this method.
+            for (int i = 0; i < ArgVars.Length; i++)
             {
                 ArgVars[i].Definition = args[i];
             }
-            return Definition.Pass();
+            //
+
+
+            return Behavior.Pass();
         }
     }
 
